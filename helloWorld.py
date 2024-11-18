8c0f2122e05b7f4fc950f11b69493269b0b90320
DelanyMusariri <delanx2005@gmail.com>
2024-11-18 19:14:20 +0200

    fixes #1: added comments

--- helloWorld.py
+++ helloWorld.py
@@ -1,3 +1,3 @@
-name = input("what is your name? ")
-print(name)
+name = input("what is your name? ")  # assigned to a variable
+print(name)  # printed out variable
 print(f"hello {name} wlecome to the world")
